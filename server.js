89dc86eeab87ea52c556ed1e96a0b1c505c9f9e8
sifatnabil <sifatnabil@gmail.com>
2021-09-27 19:25:59 +0600

    testing with webrtc

---
+++ server.js
@@ -0,0 +1,72 @@
+const Socket = require("websocket").server;
+const http = require("http");
+
+const server = http.createServer((req, res) => {});
+
+server.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
+
+const webSocket = new Socket({ httpServer: server });
+
+let users = [];
+
+webSocket.on("request", (req) => {
+  const connection = req.accept();
+
+  connection.on("message", (message) => {
+    const data = JSON.parse(message.utf8Data);
+
+    const user = findUser(data.username);
+
+    switch (data.type) {
+      case "store_user":
+        if (user != null) {
+          return;
+        }
+
+        const newUser = {
+          conn: connection,
+          username: data.username,
+        };
+        users.push(newUser);
+        console.log(newUser.username);
+        break;
+
+      case "store_offer":
+        if (user == null) return;
+
+        user.offer = data.offer;
+        break;
+
+      case "store_candidate":
+        if (user == null) return;
+
+        if (user.candidates == null) {
+          user.candidate = [];
+        }
+
+        user.candidates.push(data.candidates);
+        break;
+
+      case "send_answer":
+        if (user == null) {
+          return;
+        }
+
+        sendData(
+          {
+            type: "answer",
+            answer: data.answer,
+          },
+          user.conn
+        );
+    }
+  });
+});
+
+function findUser(username) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == username) return users[i];
+  }
+}
